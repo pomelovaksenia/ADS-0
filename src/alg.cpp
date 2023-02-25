f011068d21a45936362b9adbce70a69a296f3b3c
pomelovaksenia <116708935+pomelovaksenia@users.noreply.github.com>
2023-02-25 16:24:36 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,7 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    while (a!=0 && b!=0){
-    if (a>b){
-        if (a>b) a%=b; else b%=a;
-        return a+b;
-        }
-    }
+    while (a!=0 && b!=0)
+    if (a>b) a%=b; else b%=a;
+    return a+b;
 }
